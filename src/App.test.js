02057c96279d0455f05aacc841254ceead05d3d6
Laurent Pepin <lolocaramelos@hotmail.fr>
2026-05-03 19:42:03 +0200

    Adding form submission test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import BookingForm from "./components/BookingForm";
 import { initializeTimes, updateTimes } from "./components/BookingPage";
 
@@ -40,4 +40,42 @@ test("updateTimes returns the current state for unknown action", () => {
   const action = { type: "UNKNOWN_ACTION" };
   const newState = updateTimes(currentState, action);
   expect(newState).toEqual(currentState);
+});
+
+test("can submit the form when all required fields are filled", () => {
+  const mockOnDateChange = jest.fn();
+  const mockAvailableTimes = [
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ];
+
+  render(
+    <BookingForm
+      availableTimes={mockAvailableTimes}
+      onDateChange={mockOnDateChange}
+    />,
+  );
+
+  const dateInput = screen.getByLabelText("Choose date");
+  const timeSelect = screen.getByLabelText("Choose time");
+  const guestsInput = screen.getByLabelText(/Number of guests/);
+  const occasionSelect = screen.getByLabelText("Occasion");
+  const submitButton = screen.getByText("Make your reservation");
+
+  fireEvent.change(dateInput, { target: { value: "2026-05-10" } });
+  fireEvent.change(timeSelect, { target: { value: "18:00" } });
+  fireEvent.change(guestsInput, { target: { value: "2" } });
+  fireEvent.change(occasionSelect, { target: { value: "Birthday" } });
+
+  expect(submitButton).not.toBeDisabled();
+  fireEvent.click(submitButton);
+
+  expect(dateInput.value).toBe("");
+  expect(timeSelect.value).toBe("");
+  expect(guestsInput.value).toBe("1");
+  expect(occasionSelect.value).toBe("");
 });
